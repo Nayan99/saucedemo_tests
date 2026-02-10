1d912e8661b522407d06c76ee84ac0520c0f8312
nnekibayev <nayan@nnekibayev.local>
2026-02-10 15:57:12 +0500

    update login_page, test_login

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,22 +1,25 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 import test_data as td
 
 class LoginPage:
     USERNAME_INPUT = "#user-name"
     PASSWORD_INPUT = "#password"
     LOGIN_BUTTON = "#login-button"
-    ERROR_MESSAGE = "//h3[@data-test='error']"
+    ERROR_MESSAGE = '.error-message-container'
 
     def __init__(self, page: Page):
         self.page = page
 
     def goto(self):
-        self.page.goto(td.BASE_URL)
+        self.page.goto(td.BASE_URL, wait_until='domcontentloaded')
 
     def login(self, username: str, password: str):
         self.page.fill(self.USERNAME_INPUT, username)
         self.page.fill(self.PASSWORD_INPUT, password)
         self.page.click(self.LOGIN_BUTTON)
+        self.page.wait_for_load_state('domcontentloaded')
 
     def get_error_message(self) -> str:
-        return self.page.inner_text(self.ERROR_MESSAGE)
+        error = self.page.locator(self.ERROR_MESSAGE)
+        expect(error).to_be_visible()
+        return error.inner_text().strip()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,14 @@
+from playwright.sync_api import expect
 from pages.login_page import LoginPage
+import time
+import re
 
 def test_login_valid_user(page):
     login_page = LoginPage(page)
     login_page.goto()
     login_page.login("standard_user", "secret_sauce")
     # проверяем что попали на страницу товаров
-    assert "inventory" in page.url
+    expect(page).to_have_url(re.compile(r".*inventory.*"))
 
 
 def test_login_invalid_user(page):
@@ -13,7 +16,9 @@ def test_login_invalid_user(page):
     login_page.goto()
     login_page.login("nayan", "bayan")
     # проверяем что не залогинились
-    assert "inventory" not in page.url
+    expect(page).not_to_have_url(re.compile(r".*inventory.*"))
     # проверяем что появилось сообщение об ошибки
     error_message = login_page.get_error_message()
-    assert error_message != ""
+    assert ('Epic sadface: Username and password do not match any user in this service' in
+            error_message), 'Некорректное сообщение о некорректной авторизации'
+    time.sleep(5)
